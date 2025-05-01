7fa2d6f4930ed0dae0071aa85c050f8517ba8800
Pawin Vongmasa <pawin.vongmasa@gmail.com>
2025-05-01 16:40:24 -0700

    Clean up unused files

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,11 +3,16 @@ import { ZoomableView } from './components/zoomable-view'
 
 ZoomableView.register()
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const app = document.getElementById("app")
+if (!app) {
+  throw "app is null"
+}
+
+app.innerHTML = `
   <div id="render-area">
     <zoomable-view id="zoomable-view">
-      <div slot="background" style="width:100%; height:100%;">
-        <img src="https://images.unsplash.com/photo-1634487828605-72a3ccc3c652?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="object-fit: cover; width: 100%; height: 100%;">
+      <div slot="background" style="position: relative; width:100%; height:100%;">
+        <img src="https://images.unsplash.com/photo-1634487828605-72a3ccc3c652?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="display: block; width:100%; height:100%; object-fit: cover; object-position: center;" />
       </div>
       <div id="clock">
       </div>
